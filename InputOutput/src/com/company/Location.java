5811c5a85e7e7cd4d6247d1d0bf9994c4bad7a25
DorkinsTheOwl <toms.sokolovs@gmail.com>
2018-01-10 18:48:32 +0200

    different ways of input output

--- InputOutput/src/com/company/Location.java
+++ InputOutput/src/com/company/Location.java
@@ -1,20 +1,23 @@
 package com.company;
 
-import java.util.HashMap;
+import java.io.Serializable;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Location {
+public class Location implements Serializable {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
+
+    private long serialVersionUID = 1L;
 
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
         if(exits != null) {
-            this.exits = new HashMap<>(exits);
+            this.exits = new LinkedHashMap<>(exits);
         } else {
-            this.exits = new HashMap<>();
+            this.exits = new LinkedHashMap<>();
         }
         this.exits.put("Q", 0);
     }
@@ -32,7 +35,7 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<>(exits);
+        return new LinkedHashMap<>(exits);
     }
 
     protected void addExit(String direction, int location) {
