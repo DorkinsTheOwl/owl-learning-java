2d555aee7fcce301eb4b5a1fb352164480a36049
DorkinsTheOwl <toms.sokolovs@gmail.com>
2018-01-14 12:37:04 +0200

    more on file trees and starting on threads

---
+++ Directories/src/com/company/PrintNames.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class PrintNames extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println(dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file: " + file.toAbsolutePath() + " " + exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+}
