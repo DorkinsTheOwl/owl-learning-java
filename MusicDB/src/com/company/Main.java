6288370c3ea5e12c1ac6018b83ec070848dbb035
DorkinsTheOwl <toms.sokolovs@gmail.com>
2018-01-28 13:08:36 +0200

    more on databases in Java

--- MusicDB/src/com/company/Main.java
+++ MusicDB/src/com/company/Main.java
@@ -2,8 +2,10 @@ package com.company;
 
 import com.company.model.Artist;
 import com.company.model.Datasource;
+import com.company.model.SongArtist;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,7 +16,7 @@ public class Main {
             return;
         }
 
-        List<Artist> artists = datasource.queryArtists();
+        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_NONE);
         if (artists == null) {
             System.out.println("No artists!");
             return;
@@ -22,6 +24,49 @@ public class Main {
 
         for (Artist artist : artists) {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        System.out.println("====================================================");
+        List<String> albumsForArtists = datasource.queryAlbumsForArtist("Carole King", Datasource.ORDER_BY_ASC);
+
+        for (String album : albumsForArtists) {
+            System.out.println(album);
+        }
+
+        System.out.println("====================================================");
+        List<SongArtist> songArtists = datasource.queryArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if (songArtists == null) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() + ", Album name = " + artist.getAlbumName() +
+                    ", Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
+
+        int count = datasource.getCount(Datasource.TABLE_SONGS);
+        System.out.printf("Number of songs is: %d\n", count);
+
+        System.out.println("====================================================");
+        datasource.createViewForSongArtists();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a song title: ");
+        String title = scanner.nextLine();
+
+//        songArtists = datasource.querySongInfoView("She's on Fire");
+        songArtists = datasource.querySongInfoView(title);
+        if (songArtists.isEmpty()) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("FROM VIEW - Artist name = " + artist.getArtistName() + ", Album name = " + artist.getAlbumName() +
+                    ", Track number = " + artist.getTrack());
         }
 
         datasource.close();
